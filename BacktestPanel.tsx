bd1b708e545881cb69d3c5e34e2a2f4b572bdd3b
FF19900102 <fumiya619015@gmail.com>
2026-06-27 06:30:52 +0900

    Add files via upload

---
+++ BacktestPanel.tsx
@@ -0,0 +1,10 @@
+import BacktestPanel from '@/components/client/BacktestPanel';
+
+export default function BacktestPage(){
+  return (
+    <main className="container">
+      <h1 className="title">AIバックテスト</h1>
+      <BacktestPanel/>
+    </main>
+  );
+}
