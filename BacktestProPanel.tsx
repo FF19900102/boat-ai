ddc359affff65f1519915ca8da0f8e8d528c13b0
FF19900102 <fumiya619015@gmail.com>
2026-06-27 09:21:09 +0900

    Add files via upload

---
+++ BacktestProPanel.tsx
@@ -0,0 +1,8 @@
+import BacktestProPanel from '@/components/client/BacktestProPanel';
+
+export default function Page(){
+ return <main className="container">
+  <h1 className="title">年間バックテスト</h1>
+  <BacktestProPanel/>
+ </main>;
+}
